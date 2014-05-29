4acb3267bd82663c3ce2f2b85813e88dfe042c8d
Brian Di Palma <offler@gmail.com>
2014-05-29 19:52:56 +0100

    Add RecentMessagesActions module.

---
+++ src/RecentMessagesActions.js
@@ -0,0 +1,36 @@
+import RecentMessagesConstants from './RecentMessagesConstants';
+
+export default class {
+    constructor(recentMessagesDispatcher) {
+        this.recentMessagesDispatcher = recentMessagesDispatcher;
+    }
+
+    messageArrived(userName) {
+        this.recentMessagesDispatcher.handleServerAction({
+            actionType: RecentMessagesConstants.MESSAGE_ARRIVED,
+            userName: userName
+        });
+    }
+
+    messageSelected(userName) {
+        this.recentMessagesDispatcher.handleViewAction({
+            actionType: RecentMessagesConstants.MESSAGE_SELECTED,
+            userName: userName
+        });
+    }
+
+    userStatusChanged(userName, status) {
+        this.recentMessagesDispatcher.handleServerAction({
+            actionType: RecentMessagesConstants.USER_STATUS_CHANGED,
+            userName: userName,
+            status: status
+        });
+    }
+
+    messageListArrived(messagesState) {
+        this.recentMessagesDispatcher.handleServerAction({
+            actionType: RecentMessagesConstants.MESSAGE_LIST_ARRIVED,
+            messagesState: messagesState
+        });
+    }
+};
