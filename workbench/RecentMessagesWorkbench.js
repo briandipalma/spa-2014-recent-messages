5e3af7306255ca81625e689acb31bb825e7d6a29
Brian Di Palma <offler@gmail.com>
2014-05-30 17:07:23 +0100

    Create workbench functionality that allows population of user list.

---
+++ workbench/RecentMessagesWorkbench.js
@@ -0,0 +1,17 @@
+import {RecentMessagesElement} from "../src/RecentMessagesElement";
+
+document.registerElement("spa2014-recent-messages", RecentMessagesElement);
+
+export function initializeWorkbench() {
+    var sendButton = document.querySelector("#sendButton");
+    var recentMessagesElement = document.querySelector("spa2014-recent-messages");
+
+    var messages = new Map();
+
+    messages.set("username", {status: "online", messageOutstanding: "message-outstanding"});
+    messages.set("username2", {status: "offline", messageOutstanding: ""});
+
+    sendButton.addEventListener("click", (mouseEvent) => {
+        recentMessagesElement.recentMessagesActions.messageListArrived(messages);
+    });
+}
