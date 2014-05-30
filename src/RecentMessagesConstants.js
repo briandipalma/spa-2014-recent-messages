7f1c0649974367226031c0ef88138189a8738fa6
Brian Di Palma <offler@gmail.com>
2014-05-30 17:10:28 +0100

    Delete comments from Constants module.

--- src/RecentMessagesConstants.js
+++ src/RecentMessagesConstants.js
@@ -4,8 +4,3 @@ export default {
     USER_STATUS_CHANGED: {},
     MESSAGE_LIST_ARRIVED: {}
 }
-
-//MESSAGE_LIST_ARRIVED provides, the list of people who you are talking to. their status, arrived messages since last login.
-//USER_STATUS_CHANGED toggles the user from online to offline.
-//MESSAGE_SELECTED User clicks on one of the recent messages. If it has outstanding messages then clear that mark.
-//MESSAGE_ARRIVED a message from a contact has arrived. If the contact is selected then it's not marked as message outstanding.
