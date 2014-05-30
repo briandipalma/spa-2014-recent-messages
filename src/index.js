05c7b262779dd23754bf1ac915d0c460ee35492b
Brian Di Palma <offler@gmail.com>
2014-05-30 11:52:45 +0100

    Create a RecentMessages element and add it to the workbench.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export function printHello() {
-	console.info("Hello");
+import {Dispatcher} from "flux-es6";
+
+import RecentMessagesStore from "./RecentMessagesStore";
+import RecentMessagesActions from "./RecentMessagesActions";
+
+export function createStoreAndActions() {
+    var recentMessagesDispatcher = new Dispatcher();
+    var recentMessagesStore = new RecentMessagesStore();
+    var recentMessagesActions = new RecentMessagesActions(recentMessagesDispatcher);
+    
+    recentMessagesDispatcher.register((payload) => recentMessagesStore.handleDispatcherAction(payload));
+
+    return [recentMessagesStore, recentMessagesActions];
 }
+
+export {RecentMessagesElement} from "./RecentMessagesElement";
